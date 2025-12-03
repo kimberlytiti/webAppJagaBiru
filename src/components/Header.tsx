fc57165bcc22e35cb5ea05ea36de6ae5a12f2710
kimberlytiti <shuayoon123@gmail.com>
2025-12-03 22:37:11 +0700

    aagitig

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 'use client'
 import { useState, useEffect } from "react"
 import Link from "next/link"
+import Image from "next/image";
 import { usePathname } from 'next/navigation'
 import { Button } from "@/components/ui/button"
 import { Menu, Coins, Leaf, Search, Bell, User, ChevronDown, LogIn, LogOut } from "lucide-react"
@@ -232,13 +233,19 @@ const login = async () => {
           <Button variant="ghost" size="icon" className="mr-2 md:mr-4" onClick={onMenuClick}>
             <Menu className="h-6 w-6" />
           </Button>
-          <Link href="/" className="flex items-center">
-            <Leaf className="h-6 w-6 md:h-8 md:w-8 text-blue-500 mr-1 md:mr-2" />
-            <div className="flex flex-col">
-              <span className="font-bold text-base md:text-lg text-gray-800">Jaga Biru</span>
-              <span className="text-[8px] md:text-[10px] text-gray-500 -mt-1">ETHOnline24</span>
-            </div>
-          </Link>
+            <Link href="/" className="flex items-center">
+              <Image
+                src="/logoJagaBiru.png"
+                alt="Logo Jaga Biru"
+                width={48}
+                height={48}
+                className="h-10 w-10 md:h-12 md:w-12 mr-2 md:mr-3"
+              />
+              <div className="flex flex-col">
+                <span className="font-bold text-base md:text-lg text-gray-800">Jaga Biru</span>
+                <span className="text-[8px] md:text-[10px] text-gray-500 -mt-1">Kelompok 13</span>
+              </div>
+            </Link>
         </div>
         {!isMobile && (
           <div className="flex-1 max-w-xl mx-4">
